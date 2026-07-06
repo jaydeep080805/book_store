a10f7ed6a6ee406d72e94d64bd397253b0ddf0af
jay <jaydeep080805@gmail.com>
2026-07-06 10:37:02 +0100

    updated configs to use env variables instead of hardcoded ip

--- config/aws-config.sh
+++ config/aws-config.sh
@@ -7,10 +7,14 @@
 # # delete the container
 # docker container rm book_store_app_container
 
+source .env
+
 docker rm -f book_store_app_container
+
+docker system prune | y
 
 # build the docker container
 docker build -t book_store .
 
 # run the server
-docker run --name book_store_app_container -p 5001:5001 --network book_store_network book_store
+docker run --name book_store_app_container -p $EC2_PORT:$EC2_PORT --network book_store_network book_store
--- config/push-to-aws.sh
+++ config/push-to-aws.sh
@@ -1,17 +1,20 @@
 # echo "test"
 
+# this gets the environment variables
+source .env
+
 # copy the contents locally to the aws server
 scp -r -i ~/.ssh/jaydeep_cloud_deployment.pem \
 *.py lib requirements.txt Dockerfile seeds templates static config \
-ec2-user@52.56.152.77:~/book_store # this makes sure its sent to the book store
+ec2-user@$EC2_IP:~/book_store # this makes sure its sent to the book store
 
 # connect to the aws server
-# ssh -i ~/.ssh/jaydeep_cloud_deployment.pem ec2-user@52.56.152.77 
+# ssh -i ~/.ssh/jaydeep_cloud_deployment.pem ec2-user@$EC2_IP
 
 
 # the first EOF is to tell shell to keep reading until it reaches the EOF line
 # essentially replacing {} in programming languages
-ssh -i ~/.ssh/jaydeep_cloud_deployment.pem ec2-user@52.56.152.77 << EOF
+ssh -i ~/.ssh/jaydeep_cloud_deployment.pem ec2-user@$EC2_IP << EOF
     cd book_store
     bash config/aws-config.sh
 EOF
